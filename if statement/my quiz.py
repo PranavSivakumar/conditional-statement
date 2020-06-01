ffe2b956958423335917c6ad94f8baf3e77c1a7c
PranavSivakumar <66082471+PranavSivakumar@users.noreply.github.com>
2020-06-01 15:28:10 +0530

    Add files via upload

---
+++ if statement/my quiz.py
@@ -0,0 +1,25 @@
+print ("WELCOME TO QUIZ")
+a=int(input("enter you age"))
+b=10
+if a>b:
+    print ("welcome to senior mode")
+    print ("you will see a questions on the screen")
+    r =(input("who is the founder of space x "))
+    q= "elon musk"
+    if(r==q):
+        print("correct")
+    else:
+        print("wrong")
+        
+else:
+    print("you are in junior mode")
+    print("you will see a questions on the screen")
+    x=(input("how many functional systoms are on human body"))
+    y="11"
+    if(x==y):
+        print ("correct")
+    else:
+        print("wrong")
+
+        
+    
